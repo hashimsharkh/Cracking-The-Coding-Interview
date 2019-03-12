f66e21920fae73ff4698b0c9c2c8320eab97fb6e
hashimsharkh <hashimabusharkh@gmail.com>
2019-03-11 21:29:30 -0400

    Q11-Big O

--- TimeComplexity.java
+++ TimeComplexity.java
@@ -275,7 +275,48 @@ public class TimeComplexity
 		return sum;
 	}
 
-	
+	//Q11 - The following code prints all strings of length k where the characters are in sorted order
+	//It does this by generating all strings of length k and then checking if each is sorted.
+
+	int numChars=26;
+	void printSortedStrings(int remaining)
+	{
+		printSortedStrings(remaining,"");
+	}
+	void printSortedStrings(int remaining,String prefix)
+	{
+		if(remaining==0)
+		{
+			if(isInOrder(prefix))
+			{
+				System.out.println(prefix);	
+			}
+			else 
+			{
+				for(int i=0;i<numChars;i++)
+				{
+						char c= ithLetter(i);
+						printSortedStrings(remaining-1,prefix+c);
+				}
+			}
+		}
+	}
+	boolean isInOrder(String s)
+	{
+		for(int i=1;i<s.length();i++)
+		{
+			int prev =ithLetter(s.charAt(i-1));
+			int curr = ithLetter(s.charAt(i));
+			if(prev>curr)return false;
+
+		}
+		return true;
+
+	}
+	public static char ithLetter(int i)
+	{
+		return (char) (((int) 'a')+i);
+	}
 	public static void main(String[] args)
 	{
 		int arr[]={1,2,3,4,5,6};
