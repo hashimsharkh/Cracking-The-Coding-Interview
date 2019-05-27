deda13199d0678c8fa404e23b01c302e45028540
Hashim Abu Sharkh <hashimabusharkh@gmail.com>
2019-05-27 13:57:32 +0300

    Q1.7/Q1.8 unfinished

---
+++ Arrays and Strings/Q1.7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Cannot use 2d arrays in c++ as column is not know, can use vectors though
+//Probably runtime will be O(n^2)
+boolean rotate(vector<vector<int> > &matrix)
+{
+	//Will come back to it, Solution will be made soon
+
+}
---
+++ Arrays and Strings/Q1.8.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Q1.8
+//Will come back to it, Solution will be made soon
+
