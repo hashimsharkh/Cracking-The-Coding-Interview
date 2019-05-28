869f2e80bade523cd0625e02951d9845ad22d073
Hashim Abu Sharkh <hashimabusharkh@gmail.com>
2019-05-28 13:50:07 +0300

    Q2.1 solved

--- Arrays and Strings/Q1.9.cpp
+++ Arrays and Strings/Q1.9.cpp
@@ -6,6 +6,8 @@ using namespace std;
 //Q1.9
 //What is a rotation? Rotation at a particular point is when a string is cut at a specific character and the left side of the cut is placed at the end of the 
 //string
+
+//Assume isSubstring is a given method,do not have to implement it
 bool isSubstring(string str1,string str2)
 {
 	return true;
@@ -15,6 +17,9 @@ bool isSubstring(string str1,string str2)
 bool isRotation(string str1,string str2)
 {
 	//If they are not of the same size
+	if(str1.size()!=str2.size() && str1.size()>0)
+		return false;
+	
 	str2.append(str1);
 
 	return isSubstring(str2,str1);
