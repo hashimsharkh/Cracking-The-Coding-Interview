90ca4d210fca722cdd264d8a3880ca4ba3d12132
Hashim Abu Sharkh <hashimabusharkh@gmail.com>
2019-06-09 15:23:12 +0300

    Started solving Q2.8

---
+++ LinkedLists/Node/Q2.8/Q2.8.cpp
@@ -0,0 +1,20 @@
+//
+//
+//  Q2.8- detect the beginning of a loop in a linked list
+//
+//  Created by Hashim Abu Sharkh on 6/9/19.
+//  Copyright © 2019 Hashim Abu Sharkh. All rights reserved.
+//
+
+#include <iostream>
+
+//First thoughts- can be done using a hash table with additional space complexity O(n).
+//However, I think it can be done using additional space of O(1)
+//Using the running pointer technique, they will collide at a place, will have to figure out where they collide 
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
